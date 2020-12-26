229d2dbac16089e4c2d1b324a1cc99f4fa007bba
Ivan <ivanbrusentsev@gmail.com>
2020-12-26 17:25:15 +0300

    update users

--- src/main/java/ru/prtprt/party/entity/UserEntity.java
+++ src/main/java/ru/prtprt/party/entity/UserEntity.java
@@ -11,7 +11,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.math.BigInteger;
 
-@Entity(name = "\"user\"")
+@Entity(name = "users")
 @Getter
 @Setter
 @FieldDefaults(level = AccessLevel.PRIVATE)
